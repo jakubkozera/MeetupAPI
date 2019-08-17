dc458b65b3443b53f46d35320465f3044bae4693
Jakub Kozera <kozerakuba@gmail.com>
2019-08-17 12:31:24 +0200

    Entities add

---
+++ MeetupAPI/Entities/Lecture.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeetupAPI.Entities
+{
+    public class Lecture
+    {
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public string Topic { get; set; }
+        public string Description { get; set; }
+
+        public virtual Meetup Meetup { get; set; }
+        public int MeetupId { get; set; }
+    }
+}
