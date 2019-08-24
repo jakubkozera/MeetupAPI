7d71716947aff76de79bb9da76a476dc02274df1
Jakub Kozera <kozerakuba@gmail.com>
2019-08-24 13:24:36 +0200

    MeetupController

---
+++ MeetupAPI/Controllers/MeetupController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MeetupAPI.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeetupAPI.Controllers
+{
+    [Route("api/meetup")]
+    public class MeetupController : ControllerBase
+    {
+        private readonly MeetupContext _meetupContext;
+
+        public MeetupController(MeetupContext meetupContext)
+        {
+            _meetupContext = meetupContext;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Meetup>> Get()
+        {
+            var meetups = _meetupContext.Meetups.ToList();
+
+            return meetups;
+        }
+    }
+}
