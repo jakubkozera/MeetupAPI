00f76bdd16dd45b618b5399417be5af23b734049
Jakub Kozera <kozerakuba@gmail.com>
2020-04-11 17:40:26 +0200

    2.3 register user

---
+++ MeetupAPI/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MeetupAPI.Entities;
+using MeetupAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeetupAPI.Controllers
+{
+    [Route("api/account")]
+    public class AccountController : ControllerBase
+    {
+        private readonly MeetupContext _meetupContext;
+
+        public AccountController(MeetupContext meetupContext)
+        {
+            _meetupContext = meetupContext;
+        }
+
+        [HttpPost("register")]
+        public ActionResult Register([FromBody]RegisterUserDto registerUserDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var newUser = new User()
+            {
+                Email = registerUserDto.Email,
+                Nationality = registerUserDto.Nationality,
+                DateOfBirth = registerUserDto.DateOfBirth,
+                RoleId = registerUserDto.RoleId
+            };
+
+            _meetupContext.Users.Add(newUser);
+            _meetupContext.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
