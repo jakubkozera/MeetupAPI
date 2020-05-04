813e6322672c289378315ecfcb9068a00ec21f76
Jakub Kozera <kozerakuba@gmail.com>
2020-05-04 09:11:13 +0200

    5.4 Function method

---
+++ MeetupAPI/Controllers/ConfigController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace MeetupAPI.Controllers
+{
+    [Route("config")]
+    public class ConfigController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public ConfigController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpOptions("reload")]
+        public ActionResult Reload()
+        {
+            try
+            {
+                ((IConfigurationRoot)_configuration).Reload();
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
